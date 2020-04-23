515f4fc43b3f279bc63ee38189bd422e19e63eaf
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-04-23 02:02:06 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,37 @@
 ﻿using System;
-using System.Security.Cryptography;
 using System.Threading;
 namespace numberGuessingGame {
     class Program {
         static void Main (string[] args) {
             //C# Number Guessing Game written by Benjamin Patrick EYOH (Bhenjameen)
 
+            //Declare Variables
             string name = "";
             string level = "";
             int maxTries = 0;
-            int tries = 0;
+            string start = "";
             int min = 0;
             int max = 0;
-            int remTries = 0;
-            bool gameOver = false;
+            //int remTries = 0;
+            int gameOver = 1;
 
             Console.Write ("\n\nWelcome to Number Guessing Game. \n\nEnter your name please:-  ");
             name = Console.ReadLine ();
-            Console.WriteLine ("\n");
 
             Thread.Sleep (800);
-            Console.WriteLine ("...");
+            Console.WriteLine ("\n...");
             Thread.Sleep (800);
             Console.Write ("Hello " + name + " \nChoose your difficulty level\n\n");
-
             Thread.Sleep (800);
             Console.WriteLine ("...");
-            Thread.Sleep (800);
+
+            //Highlight and explain levels of the game
             Console.WriteLine ("easy:     (Guess between 1 and 10 with 6 guesses)");
             Console.WriteLine ("medium:   (Guess between 1 and 20 with 4 guesses)");
             Console.WriteLine ("hard:     (Guess between 1 and 50 with 3 guesses)\n");
-            Console.Write ("Type your level and press 'ENTER' to start:-  ");
+
+            //User is able to set the level they want to play
+            Console.Write ("Type your level and press 'ENTER' to continue:-  ");
             level = Console.ReadLine ();
             Console.WriteLine ();
 
@@ -46,62 +47,47 @@ namespace numberGuessingGame {
                 maxTries = 6;
                 min = 1;
                 max = 10;
-            } else {
-                Console.Write ("Type your level and press 'ENTER' to start:-  ");
-                level = Console.ReadLine ();
-                Console.WriteLine ();
             }
 
             Thread.Sleep (800);
             Console.WriteLine ("...");
             Console.WriteLine ("You chose " + level + " level");
             Console.WriteLine ("You have " + maxTries + " guesses");
+
             Thread.Sleep (800);
             Console.WriteLine ("...");
-            Thread.Sleep (800);
             Console.WriteLine ("Press 'ENTER' to start");
-            Console.ReadLine ();
-            Thread.Sleep (800);
-            Console.WriteLine ("...");
-            Thread.Sleep (800);
-            Console.Write ("I am thinking of a number between " + min + " and " + max + ". \nWhat is the number:   ");
-            string guessAsString = Console.ReadLine ();
+            start = Console.ReadLine ();
 
-            Random random = new Random ();
-            int magicNumber = random.Next (min, (max + 1));
+            //Game starts
+            while (maxTries > 0) {
+                Random random = new Random ();
+                int magicNumber = random.Next (min, max + 1);
 
-            while (gameOver == false) {
+                Console.Write ("\nI am thinking of a number between " + min + " and " + max + ". \nGuess the number :   ");
+                string guessAsString = Console.ReadLine ();
+                int guess = -1;
 
-                remTries = maxTries--;
-
-                int guess = 0;
-                if (!int.TryParse (guessAsString, out guess)) {
-                    Thread.Sleep (800);
-                    Console.WriteLine ("...");
-                    Console.WriteLine ("This is not a number. \nPlease input a number");
-                    Console.WriteLine ("...");
-                    Console.Write ("I am thinking of a number between " + min + " and " + max + ". \nWhat is the number:   ");
-                } else if (guess != magicNumber && tries == maxTries) {
-                    Thread.Sleep (800);
-                    Console.WriteLine ("...");
-                    Console.WriteLine ("Game Over!!! \nYou LOSE!!!");
-                    gameOver = true;
-                } else if (guess == magicNumber) {
-                    Thread.Sleep (800);
-                    Console.WriteLine ("...");
-                    Console.WriteLine ("You got it right. \nCongratulations " + name);
-                    gameOver = true;
-                } else if (guess != magicNumber) {
-                    Thread.Sleep (800);
-                    Console.WriteLine ("...");
-                    Console.WriteLine ("That was wrong! \nTry again " + name + ". You have " + maxTries + " guesses left");
-                    Thread.Sleep (800);
-                    Console.WriteLine ("...");
-                    Console.Write ("I am thinking of a number between " + min + " and " + max + ". \nWhat is the number:   ");
-
+                //Integer input validation and win and lose declarations
+                if (int.TryParse (guessAsString, out guess)) {
+                    if (guess == magicNumber) {
+                        Console.WriteLine ("\nYou got it right");
+                        break;
+                    } else if (maxTries <= gameOver) {
+                        Console.WriteLine ("\nThat was wrong. \nYou are out of chances. \nGame Over!!! You Lose.");
+                        break;
+                    } else {
+                        maxTries -= 1;
+                        Console.WriteLine ("\nThat was wrong, try again");
+                        Console.WriteLine ("You have " + maxTries + " guess(es) left");
+                    }
+                } else {
+                    Console.WriteLine ("\nThat is not a number. \nPlease check your input and ensure it is a number");
                 }
             }
 
         }
+
     }
+
 }
